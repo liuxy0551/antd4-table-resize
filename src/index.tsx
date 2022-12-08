45d2b3cbbf7e0f9f23a6037a04814c6188a1675a
liuxy0551 <liuxy0551@qq.com>
2022-12-08 14:56:20 +0800

    1 file modified

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import ResizableTitle from './components/resizableTitle';
 import { dataSource, getDefaultColumns, geColumntWidth } from './utils';
 
 const App: React.FC = () => {
-  const tableHeight = 600; 
+  const tableHeight = 600;
   const [form] = Form.useForm();
   const [columns, setColumns] = useState<ColumnsType>(getDefaultColumns());
   const [result, setResult] = useState([]);
@@ -69,7 +69,6 @@ const App: React.FC = () => {
         <Table
           className="dt-table-header-user-select-none"
           defaultExpandAllRows
-          bordered={false}
           components={{
             ...virtualComponent,
             header: {
